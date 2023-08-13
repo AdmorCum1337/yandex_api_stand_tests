8e554c9ec08110c76a5ded0274e93b73f963ce91
Kirill <chistofatk@gmail.com>
2023-08-14 02:24:37 +0500

    update

--- configuration.py
+++ configuration.py
@@ -4,3 +4,5 @@ LOG_MAIN_PATH = "/api/logs/main/"
 USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
 CREATE_USER_PATH = "/api/v1/users/"
 PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
+#Проверка обновление репозитория )))))
